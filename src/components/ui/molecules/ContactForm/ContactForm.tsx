0c9ed3746f83f3f5bd233c7f1c79b0a4c954387b
Ivica Batinic <ivica.batinic@infinum.hr>
2020-11-08 21:08:37 +0100

    WIP on contact form

---
+++ src/components/ui/molecules/ContactForm/ContactForm.tsx
@@ -0,0 +1,25 @@
+import { FormControl, FormLabel, Input, FormHelperText, Textarea, chakra, Button } from '@chakra-ui/core';
+import React, { FC } from 'react';
+
+export const ContactForm: FC = () => {
+	return (
+		<chakra.form maxW="xl" mx="auto">
+			<FormControl id="email" mb={5}>
+				<FormLabel>Email address</FormLabel>
+				<Input type="email" />
+				<FormHelperText>We'll never share your email.</FormHelperText>
+			</FormControl>
+			<FormControl id="subject" mb={5}>
+				<FormLabel>Subject</FormLabel>
+				<Input type="subject" />
+			</FormControl>
+			<FormControl id="message" mb={5}>
+				<FormLabel>Message</FormLabel>
+				<Textarea placeholder="Enter your message here!" />
+			</FormControl>
+			<Button colorScheme="blue" type="submit" w="100%">
+				Send
+			</Button>
+		</chakra.form>
+	);
+};
--- src/components/ui/organisms/ContactSection/ContantSection.tsx
+++ src/components/ui/organisms/ContactSection/ContantSection.tsx
@@ -1,12 +1,16 @@
 import React, { FC } from 'react';
 import { Container, Heading } from '@chakra-ui/core';
 import { Section } from '@ui/atoms/Section/Section';
+import { ContactForm } from '@ui/molecules/ContactForm/ContactForm';
 
 export const ContactSection: FC = (props) => {
 	return (
 		<Section py="10" {...props}>
 			<Container maxW="lg">
-				<Heading textAlign="center">Contact</Heading>
+				<Heading textAlign="center" mb={10}>
+					Contact
+				</Heading>
+				<ContactForm />
 			</Container>
 		</Section>
 	);
