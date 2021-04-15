563c130730e8a8ddb3634fe4e734ef77aea0c75b
Marko Mihelj <marko.mihelj@infinum.hr>
2021-04-15 12:50:32 +0200

    fix spaces

--- bin/build.sh
+++ bin/build.sh
@@ -6,15 +6,15 @@ set -o nounset
 # set -o xtrace
 
 function main() {
-  # bundle install
-  npm install
+	# bundle install
+	npm install
 
-  gem install secrets_cli
-  secrets pull -e staging -y
+	gem install secrets_cli
+	secrets pull -e staging -y
 
-  # npm run lint
-  npm run test
-  npm run build
+	# npm run lint
+	npm run test
+	npm run build
 }
 
 main "$@"
