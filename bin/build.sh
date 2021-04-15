ade57e42d0f6375e2c498cdea54bd81e78948166
Marko Mihelj <marko.mihelj@infinum.hr>
2021-04-15 12:28:54 +0200

    add deploy.sh and build.sh

---
+++ bin/build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+# set -o xtrace
+
+function main() {
+  # bundle install
+  npm install
+
+  gem install secrets_cli
+  secrets pull -e staging -y
+
+  # npm run lint
+  npm run test
+  npm run build
+
+  # RUN SONAR ANALYSIS
+  # ./bin/analyze.sh
+}
+
+main "$@"
