9c576a247f4c92cf5df20a54cab1b98be272b5e3
Marko Mihelj <marko.mihelj@infinum.hr>
2021-04-16 13:40:12 +0200

    add save next cache on semaphore

--- bin/build.sh
+++ bin/build.sh
@@ -6,8 +6,14 @@ set -o nounset
 # set -o xtrace
 
 function main() {
+	if [ ! -d "$SEMAPHORE_CACHE_DIR/.next/cache/" ]; then
+		mkdir -p "$SEMAPHORE_CACHE_DIR/.next/cache/"
+	fi
+	mkdir -p "$SEMAPHORE_PROJECT_DIR/.next/"
+	ln -s "$SEMAPHORE_CACHE_DIR/.next/cache/" "$SEMAPHORE_PROJECT_DIR/.next/cache"
+
 	# bundle install
-	npm install
+	npm ci
 
 	gem install secrets_cli
 	secrets pull -e staging -y
