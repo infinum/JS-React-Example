bce599f16b4a8790c0298fac8015ae79bbc5c6ef
kristian240 <kristian.djakovic@infinum.hr>
2021-08-24 10:24:43 +0200

    Create AuthRedirect component

--- src/hooks/useSession.ts
+++ src/hooks/useSession.ts
@@ -1,9 +1,7 @@
-import { useEffect, useMemo } from 'react';
+import { useMemo } from 'react';
 import { Response } from '@datx/jsonapi';
 import { cache, SWRConfiguration } from 'swr';
 import { useDatx, useResource } from '@/libs/@datx/jsonapi-react';
-import { UrlObject } from 'url';
-import { useRouter } from 'next/router';
 
 import { Session } from '@/resources/Session';
 import { User } from '@/resources/User';
@@ -13,18 +11,6 @@ const createSession = (store, attributes) =>
 	store.request('sessions', 'POST', attributes, { queryParams: { include: 'user' } });
 
 interface IOptions extends SWRConfiguration<Response<Session>> {
-	/**
-	 * `href` param passed to Next.js `Router.push` method
-	 */
-	redirectTo?: UrlObject | string;
-	/**
-	 * If this is set to `true` user will be redirected if he is logged in.
-	 * Useful when you don't want to show login page to already logged in users.
-	 */
-	redirectIfFound?: boolean;
-	/**
-	 * on logout success callback
-	 */
 	onLogoutSuccess?: () => void;
 	/**
 	 * on logout error callback
@@ -40,17 +26,18 @@ interface IOptions extends SWRConfiguration<Response<Session>> {
 	onLoginError?: (error: Array<IError> | Error) => void;
 }
 
-export const useSession = ({ redirectTo, redirectIfFound, onLogoutSuccess, onLogoutError, ...rest }: IOptions = {}) => {
+export const useSession = ({ onLogoutSuccess, onLogoutError, ...rest }: IOptions = {}) => {
 	const store = useDatx();
-	const router = useRouter();
 
-	const { data: session, error, mutate, isValidating } = useResource<Session>(
-		() => [Session, 'current', { queryParams: { include: 'user' } }],
-		{
-			shouldRetryOnError: false,
-			...rest,
-		}
-	);
+	const {
+		data: session,
+		error,
+		mutate,
+		isValidating,
+	} = useResource<Session>(() => [Session, 'current', { queryParams: { include: 'user' } }], {
+		shouldRetryOnError: false,
+		...rest,
+	});
 
 	const user = session?.user;
 
@@ -79,24 +66,6 @@ export const useSession = ({ redirectTo, redirectIfFound, onLogoutSuccess, onLog
 		// deps as they are new on each render!
 		[store, mutate, onLogoutError, onLogoutSuccess]
 	);
-
-	useEffect(() => {
-		// https://swr.vercel.app/advanced/performance#dependency-collection
-		const hydration = session === undefined && error === undefined && isValidating === false;
-
-		// if no redirect needed, just return (example: already on /dashboard)
-		// if user data not yet there (fetch in progress, logged in or not) then don't do anything yet
-		if (!redirectTo || hydration || isValidating) return;
-
-		if (
-			// If redirectTo is set, redirect if the user was not found.
-			(redirectTo && !redirectIfFound && !session) ||
-			// If redirectIfFound is also set, redirect if the user was found
-			(redirectIfFound && session)
-		) {
-			router.push(redirectTo);
-		}
-	}, [session, redirectIfFound, redirectTo, error, isValidating, router]);
 
 	return {
 		session,
