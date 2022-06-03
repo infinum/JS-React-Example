b703b4f1a7a69faa8fee3e97c3eacacafc827b99
Igor Lide <igor.lide@infinum.com>
2022-06-03 19:32:14 +0200

    Remove Flex component

--- src/components/shared/layouts/AuthLayout/AuthLayout.tsx
+++ src/components/shared/layouts/AuthLayout/AuthLayout.tsx
@@ -1,10 +1,9 @@
 import React, { FC } from 'react';
-import { BoxProps, Container, Flex } from '@chakra-ui/react';
+import { BoxProps, Container } from '@chakra-ui/react';
 
 export const AuthLayout: FC<BoxProps> = ({ children }) => {
 	return (
 		<Container pr={64} size="md">
-			<Flex m={4}>Test</Flex>
 			{children}
 		</Container>
 	);
