85fb0964b1eba4a1aa6e137676fcc24a3b8cc7e7
Ivica Batinic <ivica.batinic@infinum.hr>
2022-12-05 14:15:50 +0100

    Update jsonapi types

--- src/interfaces/Jsonapi.ts
+++ src/interfaces/Jsonapi.ts
@@ -1,7 +1,92 @@
 import { IModelConstructor, Model } from '@datx/core';
 import { IJsonapiModel } from '@datx/jsonapi';
 
-export type JsonapiResourceAttributes<TModel> = Partial<Omit<TModel, keyof IJsonapiModel | keyof Model>>;
+type LeastOneOf<T, U = { [K in keyof T]: Pick<T, K> }> = Partial<T> & U[keyof U];
+
+/**
+ * Where specified, a meta member can be used to include non-standard meta-information.
+ * The value of each meta member MUST be an object (a “meta object”).
+ *
+ * Any members MAY be specified within meta objects.
+ * @see https://jsonapi.org/format/#document-meta
+ */
+export type JsonapiMeta = Record<string, unknown>;
+
+/**
+ * A “resource identifier object” is an object that identifies an individual resource.
+ *
+ * @see https://jsonapi.org/format/#document-resource-identifier-objects
+ */
+export type JsonapiResourceIdentifier<TModelConstructor extends IModelConstructor> = (
+	| {
+			id: string;
+			lid: never;
+	  }
+	| {
+			id: never;
+			lid: string;
+	  }
+) & {
+	type: TModelConstructor['type'];
+	meta?: JsonapiMeta;
+};
+
+/**
+ * Resource linkage in a compound document allows a client to link together all
+ * of the included resource objects without having to GET any URLs via links.
+ *
+ * Resource linkage MUST be represented as one of the following:
+ * - null for empty to-one relationships.
+ * - an empty array ([]) for empty to-many relationships.
+ * - a single resource identifier object for non-empty to-one relationships.
+ * - an array of resource identifier objects for non-empty to-many relationships.
+ *
+ * @see https://jsonapi.org/format/#document-resource-object-linkage
+ */
+export type JsonapiResourceLinkage<TModelConstructor extends IModelConstructor> =
+	| null
+	| []
+	| JsonapiResourceIdentifier<TModelConstructor>
+	| Array<JsonapiResourceIdentifier<TModelConstructor>>;
+
+/**
+ * The value of the attributes key MUST be an object (an “attributes object”).
+ * Members of the attributes object (“attributes”) represent information about
+ * the resource object in which it’s defined.
+ *
+ * @see https://jsonapi.org/format/#document-resource-object-attributes
+ */
+export type JsonapiResourceAttributes<TModel> = Partial<
+	Omit<
+		{
+			[Key in keyof TModel as TModel[Key] extends IJsonapiModel | Array<IJsonapiModel> ? never : Key]-?: TModel[Key];
+		},
+		keyof IJsonapiModel | keyof Model
+	>
+>;
+
+/**
+ * The value of the relationships key MUST be an object (a “relationships object”).
+ * Each member of a relationships object represents a “relationship” from the
+ * resource object in which it has been defined to other resource objects.
+ *
+ * Relationships may be to-one or to-many.
+ *
+ * @see https://jsonapi.org/format/#document-resource-object-relationships
+ */
+export type JsonapiResourceRelationships<TModel> = Partial<
+	Omit<
+		{
+			[Key in keyof TModel as TModel[Key] extends IJsonapiModel | Array<IJsonapiModel> ? Key : never]-?: LeastOneOf<{
+				links: any;
+				// TODO: figure out support for relationship links and how to get instance type for JsonapiResourceLinkage
+				data: Record<string, unknown> | Array<Record<string, unknown>>;
+				meta: JsonapiMeta;
+			}>;
+		},
+		keyof IJsonapiModel | keyof Model
+	>
+>;
 
 /**
  * “Resource objects” appear in a JSON:API document to represent resources.
@@ -13,6 +98,7 @@ export type JsonapiResource<TModelConstructor extends IModelConstructor> = {
 	lid?: string;
 	type: TModelConstructor['type'];
 	attributes: JsonapiResourceAttributes<InstanceType<TModelConstructor>>;
+	relationships?: JsonapiResourceRelationships<InstanceType<TModelConstructor>>;
 };
 
 /**
