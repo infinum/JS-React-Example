a96d3fbc17b3ea37655105117180f6bf12c2dba9
Ivica Batinic <ivica.batinic@infinum.hr>
2022-12-07 16:47:18 +0100

    Implement ColorModeScript in _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,8 @@
+import { ColorModeScript } from '@chakra-ui/react';
 import { getSafeLocale } from '@/utils/locale';
 import { DocumentProps, Head, Html, Main, NextScript } from 'next/document';
+
+import theme from '../styles/theme/index';
 
 const AppDocument = (props: DocumentProps) => {
 	const currentLocale = getSafeLocale(props.__NEXT_DATA__.locale);
@@ -10,6 +13,7 @@ const AppDocument = (props: DocumentProps) => {
 				<meta charSet="utf-8" />
 			</Head>
 			<body>
+				<ColorModeScript initialColorMode={theme.config.initialColorMode} />
 				<Main />
 				<NextScript />
 			</body>
