c2c0ff71453721e39f92d89665177f7da52ebde0
Ivica Batinic <ivica.batinic@infinum.hr>
2022-12-12 14:18:11 +0100

    Implement next font optimization

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,14 @@ import '@datx/core/disable-mobx';
 
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
-import { createFetcher, DatxProvider, useInitialize } from '@datx/swr';
-import { createClient } from '../datx/create-client';
 import { appWithTranslation } from 'next-i18next';
+import { createFetcher, DatxProvider, useInitialize } from '@datx/swr';
 import { SWRConfig } from 'swr';
-import nextI18nConfig from '../../next-i18next.config';
-import { start } from '@/lib/bugsnag';
 
+import { createClient } from '@/datx/create-client';
+import { start } from '@/lib/bugsnag';
 import theme from '@/styles/theme';
-import { Fonts } from '@/styles/Fonts';
+import nextI18nConfig from '../../next-i18next.config';
 
 import 'focus-visible/dist/focus-visible';
 
@@ -31,7 +30,6 @@ function ExampleApp({ Component, pageProps, err }: IExampleAppProps) {
 						fetcher: createFetcher(client),
 					}}
 				>
-					<Fonts />
 					{/* Pass err as a workaround for https://github.com/vercel/next.js/issues/8592 */}
 					<Component {...pageProps} err={err} />
 				</SWRConfig>
