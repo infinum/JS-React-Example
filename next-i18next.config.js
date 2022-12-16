4ab47813107e900717ffce17ca6f49a8fe35b6a7
Ivica Batinic <ivica.batinic@infinum.hr>
2022-12-16 12:17:17 +0100

    Experimenting with csr translations

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,15 +1,25 @@
+const HttpBackend = require('i18next-http-backend/cjs');
+const ChainedBackend = require('i18next-chained-backend').default;
+const LocalStorageBackend = require('i18next-localstorage-backend').default;
+
+const isBrowser = typeof window !== 'undefined';
+
 /**
  * @type {import('next-i18next').UserConfig}
  */
 module.exports = {
 	// https://www.i18next.com/overview/configuration-options#logging
 	// debug: process.env.NODE_ENV === 'development',
+	backend: {
+		backendOptions: [{ expirationTime: 60 * 60 * 1000 }, {}], // 1 hour
+		backends: isBrowser ? [LocalStorageBackend, HttpBackend] : [],
+	},
 	i18n: {
 		defaultLocale: 'en-US',
 		locales: ['en-US', 'de-DE'],
 	},
 	/** To avoid issues when deploying to some paas (vercel...) */
-	localePath: typeof window === 'undefined' ? require('path').resolve('./public/locales') : '/locales',
+	localePath: !isBrowser ? require('path').resolve('./public/locales') : '/locales',
 	reloadOnPrerender: process.env.NODE_ENV === 'development',
 
 	/**
@@ -17,6 +27,10 @@ module.exports = {
 	 */
 	// saveMissing: false,
 	// strictMode: true,
-	// serializeConfig: false,
-	// react: { useSuspense: false }
+	serializeConfig: false,
+	// react: { // used only for the lazy reload
+	//   bindI18n: 'languageChanged loaded',
+	//   useSuspense: false
+	// },
+	use: isBrowser ? [ChainedBackend] : [],
 };
