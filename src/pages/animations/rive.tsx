3b70210205ab98c4c7ea9123904729fe358ca5af
Ivica Batinic <ivica.batinic@infinum.hr>
2023-03-08 14:41:09 +0100

    Implement Rive animation artboards

--- src/pages/animations/rive.tsx
+++ src/pages/animations/rive.tsx
@@ -1,13 +1,34 @@
+import { useState } from 'react';
 import { NextPage } from 'next';
 import Rive from '@rive-app/react-canvas';
-import { Box } from '@chakra-ui/react';
+import { Box, Button, ButtonGroup } from '@chakra-ui/react';
 import { Board } from '@/components/features/animations/Board/Board';
 
+const artboards = ['Art1', 'Art2', 'Art3'];
+
 const RiveAnimationPage: NextPage = () => {
+	const [selectedArtboard, setSelectedArtboard] = useState(artboards[0]);
+
 	return (
 		<Board w="100vw" h="100vh">
+			<ButtonGroup>
+				{artboards.map((artboard) => (
+					<Button
+						colorScheme="blue"
+						isActive={selectedArtboard === artboard}
+						onClick={() => setSelectedArtboard(artboard)}
+					>
+						{artboard}
+					</Button>
+				))}
+			</ButtonGroup>
 			<Box boxSize={300}>
-				<Rive src="/animations/rive/bloodbourne.riv" animations="C1" />
+				<Rive
+					key={selectedArtboard}
+					src="/animations/rive/bloodbourne.riv"
+					artboard={selectedArtboard}
+					animations={['A1', 'B1', 'C1']}
+				/>
 			</Box>
 		</Board>
 	);
