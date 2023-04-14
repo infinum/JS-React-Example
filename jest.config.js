eb2ebff24f0550562d6031132558bda97ab63a22
Ivica Batinic <ivica.batinic@infinum.hr>
2023-04-14 09:13:25 +0200

    add html coverage export

--- jest.config.js
+++ jest.config.js
@@ -20,6 +20,7 @@ const customJestConfig = {
 	// if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
 	moduleDirectories: ['node_modules', '<rootDir>/'],
 	testEnvironment: '@infinum/jest/environment',
+	coverageReporters: ['html'],
 	collectCoverageFrom: ['./src/**/*.{js,jsx,ts,tsx}'],
 	watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname'],
 };
