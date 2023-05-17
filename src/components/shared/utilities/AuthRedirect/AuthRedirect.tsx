fa70c4e93517ed212699bbae21a95b47a14c8c53
Ivica Batinic <ivica.batinic@infinum.hr>
2023-05-17 12:04:09 +0200

    Disable eslint warning

--- src/components/shared/utilities/AuthRedirect/AuthRedirect.tsx
+++ src/components/shared/utilities/AuthRedirect/AuthRedirect.tsx
@@ -26,27 +26,31 @@ export const AuthRedirect: FC<IAuthRedirectProps> = ({ to, ifFound, condition })
 	const { data, isValidating, error } = useSession();
 	const { push } = useRouter();
 
-	useEffect(() => {
-		// if state is validating, wait until request is done
-		if (isValidating) {
-			return;
-		}
+	useEffect(
+		() => {
+			// if state is validating, wait until request is done
+			if (isValidating) {
+				return;
+			}
 
-		// https://swr.vercel.app/advanced/performance#dependency-collection
-		const hydration = data === undefined && error === undefined && isValidating === false;
+			// https://swr.vercel.app/advanced/performance#dependency-collection
+			const hydration = data === undefined && error === undefined && isValidating === false;
 
-		// if user data not yet there (fetch in progress, logged in or not) then don't do anything yet
-		if (hydration) {
-			return;
-		}
+			// if user data not yet there (fetch in progress, logged in or not) then don't do anything yet
+			if (hydration) {
+				return;
+			}
 
-		// `condition` has a priority over a `ifFound` property
-		const shouldRedirect = condition ? condition(data?.data) : (ifFound && data) || (!ifFound && !data);
+			// `condition` has a priority over a `ifFound` property
+			const shouldRedirect = condition ? condition(data?.data) : (ifFound && data) || (!ifFound && !data);
 
-		if (shouldRedirect) {
-			push(to);
-		}
-	}, [data, ifFound, to, error, isValidating]);
+			if (shouldRedirect) {
+				push(to);
+			}
+		},
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+		[data, ifFound, to, error, isValidating]
+	);
 
 	// this component renders nothing since it is only used to redirect if needed
 	return null;
