2ef2215c88e29295326e4e3541f305247040b8bc
Kristian Djaković <kristian.djakovic@infinum.hr>
2023-07-12 10:43:11 +0200

    Write AuthLayout test

---
+++ src/components/shared/layouts/AuthLayout/AuthLayout.test.tsx
@@ -0,0 +1,31 @@
+import { axe } from 'jest-axe';
+import { render, screen } from 'test-utils';
+import { AuthLayout } from './AuthLayout';
+
+describe('AuthLayout', () => {
+	it('should matches snapshot', () => {
+		const { asFragment } = render(<AuthLayout />);
+
+		expect(asFragment()).toMatchSnapshot();
+	});
+
+	it('should render children', () => {
+		-render(
+			<AuthLayout>
+				<p>test</p>
+			</AuthLayout>
+		);
+
+		expect(screen.getByText('test')).toBeInTheDocument();
+	});
+
+	it('should be accessible', async () => {
+		const { container } = render(<AuthLayout />);
+
+		// await act(async () => {
+		const results = await axe(container);
+
+		expect(results).toHaveNoViolations();
+		// });
+	});
+});
