dba65bd2b66dab96b3a6b03f640823730a5677e8
Domen Lisjak <domen.lisjak@infinum.com>
2023-07-27 16:24:45 +0200

    Rename constant test name

--- __mocks__/constants.ts
+++ __mocks__/constants.ts
@@ -2,4 +2,4 @@ export const TEST_INVALID_EMAIL = 'Email User not found';
 
 export const TEST_INVALID_PASSWORD = 'Password Invalid password';
 
-export const TEST_FLIGHT_NAME = 'Password Invalid password';
+export const TEST_FLIGHT_NAME = 'Air Force One';
