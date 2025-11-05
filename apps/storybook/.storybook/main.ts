6d315a07f606c82721e0893378180f5335378230
Kamil Dubiel <kamil.dubiel@infinum.com>
2025-11-05 17:18:16 +0100

    Remove ESM require from Storybook main.ts

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -1,14 +1,5 @@
 import type { StorybookConfig } from '@storybook/nextjs';
 
-import { dirname, join } from 'path';
-
-/**
- * This function is used to resolve the absolute path of a package.
- * It is needed in projects that use Yarn PnP or are set up within a monorepo.
- */
-function getAbsolutePath(value: string): any {
-	return dirname(require.resolve(join(value, 'package.json')));
-}
 const config: StorybookConfig = {
 	stories: [
 		'../src/stories/**/*.mdx',
@@ -17,7 +8,7 @@ const config: StorybookConfig = {
 	],
 	addons: ['@storybook/addon-themes', '@storybook/addon-docs', '@storybook/addon-a11y', '@storybook/addon-designs'],
 	framework: {
-		name: getAbsolutePath('@storybook/nextjs'),
+		name: '@storybook/nextjs',
 		options: {},
 	},
 	core: {
